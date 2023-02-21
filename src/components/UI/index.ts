a54041d2ad6b028e42bcf841d768052b12f97f62
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-02-21 20:30:43 -0300

    media query para as margens do Container

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,8 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-padding: 70px 150px;
-min-height: calc(100vh - 70px - 59px);
+  padding: 70px 150px;
+  min-height: calc(100vh - 70px - 59px);
+  @media (max-width: 950px) {
+    padding: 70px 50px;
+  }
+  @media (max-width: 650px) {
+    padding: 70px 10px;
+  }
 `
 
 export const Subtitle = styled.h2`
