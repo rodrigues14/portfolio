08da1a4cc1188316fe23bfb755cf6e3dd8ea4c3e
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-03-03 22:29:34 -0300

    estilizado not found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,10 +1,23 @@
+import styled from "styled-components";
 import { Container } from "../../components/UI";
 import { Title } from "../Home/style";
+import error from './error404.svg'
+
+const Error404 = styled.div`
+  img {
+    max-height: 400px;
+    display: block;
+    margin-inline: auto;
+  }
+`;
 
 export default function NotFound() {
   return (
     <Container>
-      <Title>Página não encontrada!</Title>
+      <Title>Ops... <br /> Página não encontrada!</Title>
+      <Error404>
+        <img src={error} alt="Erro 404, página não encontrada" />
+      </Error404>
     </Container>
   )
 }
